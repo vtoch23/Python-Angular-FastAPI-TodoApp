3a23cdd07d91ed97c0774e44c75b5bede6683c04
Vanya <vtochevska@gmail.com>
2024-10-11 13:40:40 +0100

    added ui files

---
+++ ui/todo/src/app/app.component.ts
@@ -0,0 +1,62 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+import { CommonModule } from '@angular/common';
+import { HttpClientModule, HttpClient} from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [
+    CommonModule,
+    FormsModule,
+    HttpClientModule, 
+    ReactiveFormsModule,
+    RouterOutlet
+  ],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'todo';
+  tasks: any = [];
+  newTask = "";
+
+  APIURL = "http://localhost:8000/";
+
+  constructor(private http: HttpClient){}
+
+  ngOnInit(){
+    this.get_tasks();
+  }
+
+  get_tasks() {
+    this.http.get(this.APIURL+"get_tasks").subscribe((res) => {
+      this.tasks=res;
+    })
+  }
+
+  add_task() {
+    let body = new FormData();
+    if (this.newTask) {
+      body.append('task', this.newTask);
+      this.http.post(this.APIURL+"add_task", body).subscribe((res) => {
+        this.newTask = "";
+        this.get_tasks();
+      })
+    }
+    else {
+      alert('Please enter a task')
+    }
+  }
+
+  delete_task(id: any){
+    let body = new FormData();
+    body.append('id', id);
+    this.http.post(this.APIURL+"delete_task", body).subscribe((res) => {
+      this.get_tasks();
+    })
+  }
+}
